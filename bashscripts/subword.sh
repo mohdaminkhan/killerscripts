cb4bd86374e44b3ebf21c3e36dd164aeaffcf109
amin khan <mohdaminkhan@hotmail.com>
2023-01-31 23:29:32 -0800

    passing in arguments , making file parth absolute

--- bashscripts/subword.sh
+++ bashscripts/subword.sh
@@ -7,7 +7,7 @@
 options=$@
 clear
 
-subwordnow () {
+swapWord () {
 # We have to declare OPTIND locally, to reset this global variable, otherwise
 # the index of the loop is kept globally and will always be the end of the array
 # after the first run, meaning the script would only work once per session
@@ -18,19 +18,26 @@ while getopts "t:s:f:" flags;do
 	case $flags in
 	t) 
 	echo "t value is $OPTARG"
+	targetWord=$OPTARG
 	;;
 	s) 
 	echo "s value is $OPTARG"
+	substituteWord=$OPTARG
 	;;
 	f) 
 	echo "f value is $OPTARG"
+	fileToCheck=$(readlink -f $OPTARG)
 	;;
-	*) 
+	?) 
 	echo "Please supply t,s or f"
 	;;
 esac
+
+echo -e "\n"
 done
-echo $options
+echo 
+echo $fileToCheck
+echo $(grep $targetWord $fileToCheck)
 }
 #  we want to loop over the files passed in.
 #	for passedIn in $@;
@@ -40,5 +47,4 @@ echo $options
 #		echo -e "\n"
 #	done
 echo "this is passed in... $@"
-subwordnow "$@"
-
+swapWord "$@"
