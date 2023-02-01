7ecf608f0b2760f93783a3b730bc240ba2e66b32
amin khan <mohdaminkhan@hotmail.com>
2023-01-31 18:39:01 -0800

    modify subwordnow to work on every run by resetting env variable OPTIND that tracks index of looped array

--- bashscripts/subword.sh
+++ bashscripts/subword.sh
@@ -4,29 +4,34 @@
 
 # Gather arguments from flags using 'getopts'
 
+options=$@
+clear
 
+subwordnow () {
+# We have to declare OPTIND locally, to reset this global variable, otherwise
+# the index of the loop is kept globally and will always be the end of the array
+# after the first run, meaning the script would only work once per session
 
-# Let's look  at how command line arguments are dealt with in bash.
+declare OPTIND
 
-# Bash collects the strings passed into the  command that executes a command or
-# scripts
-
-
-# getopts is a command that parses short options (with a single dash) can also
-# parse short options in combination in. Cannot parse options with long names,
-# use 'getopt' instead. 
-
-# $@ stores the arguments in string form, getopts parses that string assigning
-# the options to their respective flags.
-
-
-# Simple echo of getopts
-
-while getopts "t:s:f:" options;do
-echo  "$options" 
+while getopts "t:s:f:" flags;do
+	case $flags in
+	t) 
+	echo "t value is $OPTARG"
+	;;
+	s) 
+	echo "s value is $OPTARG"
+	;;
+	f) 
+	echo "f value is $OPTARG"
+	;;
+	*) 
+	echo "Please supply t,s or f"
+	;;
+esac
 done
-
 echo $options
+}
 #  we want to loop over the files passed in.
 #	for passedIn in $@;
 #	do
@@ -34,4 +39,6 @@ echo $options
 #		grep Mary $passedIn
 #		echo -e "\n"
 #	done
+echo "this is passed in... $@"
+subwordnow "$@"
 
