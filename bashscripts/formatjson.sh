9417604459e492ca60b8c9ba7799cbcc2167944a
amin khan <mohdaminkhan@hotmail.com>
2023-02-07 22:48:18 -0800

    format json file created

---
+++ bashscripts/formatjson.sh
@@ -0,0 +1,13 @@
+# This script is designed to take a json input file and format it into something
+# which presents the results in a easily readable, tabulated manner.
+# Intention is to use awk, sed and grep  among  other commands to achieve this.
+
+# An input file should be provided and a resultant file  should be output.
+
+
+# Parameters: Input file, output file, tabulation format.
+
+
+
+
+
