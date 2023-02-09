7dc498c5f27dba4ebb91945fe1eedc2238c2e44e
amin khan <mohdaminkhan@hotmail.com>
2023-02-08 23:53:56 -0800

    added comments

--- bashscripts/test.sh
+++ bashscripts/test.sh
@@ -1,5 +1,13 @@
 # parse out the enclosing brackets.
 
-sed  -in  '1,$ s/(^[)(]$)/ /g' $1
+# Quotes.
+
+# ' single quotes will escape characters
+# " double  quotes will expand variables.
+# ` back ticks will run command execution, just like enclosing in $()
+#  command substitution will try to expand the variable and then run it as a
+#  command.  
+#  No quotes will print and expand variables and do command substitution but
+#  will not escape chaaracters even with -e flag   
 
 
