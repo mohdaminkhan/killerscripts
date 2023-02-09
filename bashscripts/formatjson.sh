8835443a1d8e1b0d2df2d9bd086d901fb9c78735
amin khan <mohdaminkhan@hotmail.com>
2023-02-09 15:30:37 -0800

    building out bash script, add parameter flags

--- bashscripts/formatjson.sh
+++ bashscripts/formatjson.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+
+
+
+
 # This script is designed to take a json input file and format it into something
 # which presents the results in a easily readable, tabulated manner.
 # Intention is to use awk, sed and grep  among  other commands to achieve this.
@@ -7,8 +13,55 @@
 
 # Parameters: Input file, output file, tabulation format.
 
+# tabulation format can be indented or table - output as text or csv.
+
+
+# Function to check if argument is a file location
+
+checkFile() {
+	if [[ ! -f $1 ]]
+	then
+		printf '%-6s' "this is text"
+
+	else 
+		printf "That is a file"
+	fi
+
+}
+	
 
 
 
 
+# declare variables that may be used throughout
+declare OPTIND # This is iterator in getopts
+declare isfile=true
+declare GREEN="\e[32m"
+declare lightGREEN="\e[32m"
+declare lightRED="\e[91m"
+declare lightYELLOW="\e[93m"
+declare YELLOW="\e[33j"
+declare ENDCOLOR="\e[0m"
+declare inputFile=null
+declare outputFile=null
+declare format="table"
 
+
+# get options passed in using getopts
+# for 
+while getopts "i:o:f:" flags; do 
+	case $flags in
+		i) checkFile $OPTARGS
+			;;
+		o) checkFile $OPTARGS
+			;;
+		f)checkFile $OPTARGS
+			;;
+		?)checkFile $OPTARGS
+
+
+	esac
+
+done
+
+printf "The script is finished now \n"
